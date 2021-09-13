4456a59ea8763e92b9494425babd74555d394161
ashrafulsarkar <ashrafulsarkar47@gmail.com>
2021-09-13 12:43:31 +0600

    problem fixed

--- js/app.js
+++ js/app.js
@@ -1,80 +1,125 @@
 const loadProducts = () => {
-  const url = `https://fakestoreapi.com/products`;
-  fetch(url)
-    .then((response) => response.json())
-    .then((data) => showProducts(data));
+	const url = `https://fakestoreapi.com/products`;
+	fetch(url)
+		.then((response) => response.json())
+		.then((data) => showProducts(data));
 };
 loadProducts();
 
 // show all product in UI 
 const showProducts = (products) => {
-  const allProducts = products.map((pd) => pd);
-  for (const product of allProducts) {
-    const image = product.images;
-    const div = document.createElement("div");
-    div.classList.add("product");
-    div.innerHTML = `<div class="single-product">
-      <div>
-    <img class="product-image" src=${image}></img>
-      </div>
-      <h3>${product.title}</h3>
-      <p>Category: ${product.category}</p>
-      <h2>Price: $ ${product.price}</h2>
-      <button onclick="addToCart(${product.id},${product.price})" id="addToCart-btn" class="buy-now btn btn-success">add to cart</button>
-      <button id="details-btn" class="btn btn-danger">Details</button></div>
+	const allProducts = products.map((pd) => pd);
+	for (const product of allProducts) {
+		const image = product.image;
+		const div = document.createElement("div");
+		div.classList.add("product");
+		div.innerHTML = `
+		<div class="single-product">
+		<div>
+			<img class="product-image" src=${image}></img>
+		</div>
+		<div class="product-details">
+			<div>
+				<h3 class="product-title">${product.title}</h3>
+				<p>Category: ${product.category}</p>
+			</div>
+			<div>
+				<h3 class="ratting">Ratting: ${product.rating.rate}(${product.rating.count})</h3>
+				<h2 class="price">Price: $ ${product.price}</h2>
+				<button onclick="addToCart(${product.id},${product.price})" id="addToCart-btn" class="buy-now btn btn-success">Add to cart</button>
+				<button type="button" onclick="productDetails(${product.id})" class="btn btn-danger">Details</button>
+			</div>
+		</div>
+	  </div>
       `;
-    document.getElementById("all-products").appendChild(div);
-  }
+		document.getElementById("all-products").appendChild(div);
+
+	}
 };
 let count = 0;
 const addToCart = (id, price) => {
-  count = count + 1;
-  updatePrice("price", price);
+	count = count + 1;
+	updatePrice("price", price);
 
-  updateTaxAndCharge();
-  document.getElementById("total-Products").innerText = count;
+	updateTaxAndCharge();
+	document.getElementById("total-Products").innerText = count;
+
+	updateTotal();
 };
 
 const getInputValue = (id) => {
-  const element = document.getElementById(id).innerText;
-  const converted = parseInt(element);
-  return converted;
+	const element = document.getElementById(id).innerText;
+	const converted = parseFloat(element);
+	return converted;
 };
 
 // main price update function
 const updatePrice = (id, value) => {
-  const convertedOldPrice = getInputValue(id);
-  const convertPrice = parseFloat(value);
-  const total = convertedOldPrice + convertPrice;
-  document.getElementById(id).innerText = Math.round(total);
+	const convertedOldPrice = getInputValue(id);
+	const convertPrice = parseFloat(value);
+	const total = convertedOldPrice + convertPrice;
+	document.getElementById(id).innerText = total.toFixed(2);
 };
 
 // set innerText function
 const setInnerText = (id, value) => {
-  document.getElementById(id).innerText = Math.round(value);
+	document.getElementById(id).innerText = Math.round(value);
 };
 
 // update delivery charge and total Tax
 const updateTaxAndCharge = () => {
-  const priceConverted = getInputValue("price");
-  if (priceConverted > 200) {
-    setInnerText("delivery-charge", 30);
-    setInnerText("total-tax", priceConverted * 0.2);
-  }
-  if (priceConverted > 400) {
-    setInnerText("delivery-charge", 50);
-    setInnerText("total-tax", priceConverted * 0.3);
-  }
-  if (priceConverted > 500) {
-    setInnerText("delivery-charge", 60);
-    setInnerText("total-tax", priceConverted * 0.4);
-  }
+	const priceConverted = getInputValue("price");
+	if (priceConverted > 200) {
+		setInnerText("delivery-charge", 30);
+		setInnerText("total-tax", priceConverted * 0.2);
+	}
+	if (priceConverted > 400) {
+		setInnerText("delivery-charge", 50);
+		setInnerText("total-tax", priceConverted * 0.3);
+	}
+	if (priceConverted > 500) {
+		setInnerText("delivery-charge", 60);
+		setInnerText("total-tax", priceConverted * 0.4);
+	}
 };
 
 //grandTotal update function
 const updateTotal = () => {
-  const grandTotal =
-    getInputValue("price") + getInputValue("delivery-charge") +
-    getInputValue("total-tax");
-  document.getElementById("total").innerText = grandTotal;
+	const grandTotal =
+		getInputValue("price") + getInputValue("delivery-charge") +
+		getInputValue("total-tax");
+	document.getElementById("total").innerText = grandTotal.toFixed(2);
 };
+
+//product details
+const productDetails = (id) => {
+	const modal = document.getElementById('modal');
+	modal.innerHTML = "";
+	const url = `https://fakestoreapi.com/products/${id}`;
+	fetch(url)
+		.then((response) => response.json())
+		.then((data) => modalShow(data));
+}
+
+const modalShow = (data) => {
+	const modal = document.getElementById('modal');
+	const modalContent = `
+	<!-- Modal -->
+	<div class="modal fade" id="details-btn-${data.id}" tabindex="-1" aria-hidden="true">
+		<div class="modal-dialog">
+			<div class="modal-content">
+			<div class="modal-header">
+				<h5 class="modal-title">${data.title}</h5>
+				<button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+			</div>
+				<div class="modal-body">
+					<p>${data.description}</p>
+				</div>
+			</div>
+		</div>
+	</div>
+	`;
+	modal.innerHTML = modalContent;
+	let myModal = new bootstrap.Modal(document.getElementById(`details-btn-${data.id}`));
+	myModal.toggle();
+}
